8f9713738eaff8e96a6986ae7008ae0e60b543ec
Waalfegor <100232625+Waalfegor@users.noreply.github.com>
2022-04-25 21:42:44 +0300

    begining

--- Func.cpp
+++ Func.cpp
@@ -29,48 +29,6 @@ void create_list(Node*& L, int n)
 	}
 }
 
-void sort_list(Node*& L)
-{
-	if (!L) return;
-	int p = findMinLowestDigitPos(L);
-	Node* node = findByPos(L, p);
-	if (p > 1) findByPos(L, p - 1)->next = node->next;
-	else L = node->next;
-	Node* L2 = node;
-	Node* pasteTo = L2;
-	while (true) {
-		p = findMinLowestDigitPos(L);
-		node = findByPos(L, p);
-		if (!node) break;
-		if (p > 1) findByPos(L, p - 1)->next = node->next;
-		else L = node->next;
-		pasteTo->next = node;
-		pasteTo = node;
-	}
-	L = L2;
-}
-
-int findMinLowestDigitPos(Node* L, int startFrom) 
-{
-	if (startFrom < 1) startFrom = 1;
-	Node* q = L;
-	int min = INT_MAX;
-	int p = -1;
-	for (int i = 1; i < startFrom; i++)
-	{
-		q = q->next;
-	}
-	for (int i = startFrom; q != 0; i++, q = q->next) 
-	{
-		if (abs(q->number % 10) < min || i == 1) 
-		{
-			min = abs(q->number % 10);
-			p = i;
-		}
-	}
-	return p;
-}
-
 void print(Node* L) 
 {
 	if (!L) {
@@ -85,14 +43,6 @@ void print(Node* L)
 	cout << endl;
 }
 
-bool isSorted(Node* L) 
-{
-	for (int i = 1, p = findMinLowestDigitPos(L, i); p > 0; i++, p = findMinLowestDigitPos(L, i))
-	{
-		if (i != p) return false;
-	}
-	return true;
-}
 
 int deleteByPos(Node*& L, int p)
 {
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 ﻿#include "Func.h"
 
-int main() {
+int main()
+{
 	setlocale(LC_ALL, "RUS");
 	srand(time(0));
 
@@ -13,28 +14,6 @@ int main() {
 	create_list(L, n);
 	cout << "Список:\n";
 	print(L);
-
-	//cout << "Отсортирован ли список по возрастанию?\n";
-	//cout << (isSorted(L) ? "ДА" : "НЕТ") << endl;
-
-	//int pos = 2;
-	//cout << "Значение в позиции " << pos << ":\n";
-	//cout << findByPos(L, 2)->number << endl;
-	//pos = findMinLowestDigitPos(L);
-
-	//cout << "Элемент списка с минимальным значением младшей цифры:\n";
-	//cout << pos << " " << findByPos(L, pos)->number << endl;
-
-	//sort_list(L);
-	//cout << "Отсортированный список по возрастанию значения младшей цифры:\n";
-	//print(L);
-
-	//cout << "Отсортирован ли список по возрастанию?\n";
-	//cout << (isSorted(L) ? "ДА" : "НЕТ") << endl;
-
-	//cout << "Удаление узла с позицией 1\n";
-	//deleteByPos(L, 1);
-	//print(L);
 
 	cout << "Удаление узла с позицией 2\n";
 	deleteByPos(L, 2);
